574be98a93c6dfd44d8ba6f4403721da1c205a53
dilipsa <dilipsapkota.d@gmail.com>
2021-02-22 09:09:40 +0530

    encription start

--- server.py
+++ server.py
@@ -1,19 +1,37 @@
 #!/usr/bin/python           # This is server.py file
 
 import socket 
-import os              # Import socket module
+import os    
+import sqlite3   
+conn = sqlite3.connect('new.db')
+cur = conn.cursor()
+print('connection')
+	
+try:
+	cur.execute('create table ip_data (id integer primary key autoincrement ,ip varchar(100), data varchar(200))')
+	pass
+except:
+	print("table alreay created")       # Import socket module
 
 s = socket.socket()         # Create a socket object
 # host = socket.gethostname() # Get local machine name
 host = '192.168.0.142'
-port = 12345                # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
-s.listen(5)                 # Now wait for client connection.
+port = 12345               # Reserve a port for your service.
+# s.bind((host, port))        # Bind to the port
+s.listen(5)  
+print('host is lisening at ', host , port)               # Now wait for client connection.
 while True:
    c, addr = s.accept()     # Establish connection with client.
-   print(b'Got connection from', addr)
+   print('Got connection from', addr)
+   print(addr)
    msg = c.recv(1024)
    print(msg)
-   os.mkdir(msg)
+   if msg == b'decrpyt':
+   		cur.execute('select data from ip_data where ip = "{}"'.format(addr[0]))
+   		print(cur.fetchall())
+   else:
+   		cur.execute('insert into ip_data (ip,data) values ("{}","{}")'.format(addr[0],msg))
+   		conn.commit()
+   #os.mkdir(msg)
    c.send(b'Thank you for connecting')
    c.close()                # Close the connection
